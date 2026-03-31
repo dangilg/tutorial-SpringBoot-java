13f45d070118a847f68b442b62c96023971acbcc
Daniel Gil <dangilgut@gmail.com>
2026-03-31 09:42:28 +0200

    fix: actualización de la Exception lazada cuando se realiza una operación con un ID q no existe

--- src/main/java/com/ccsw/tutorial/exceptions/NoIdFoundException.java
+++ src/main/java/com/ccsw/tutorial/exceptions/NoIdFoundException.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NoIdFoundException extends RuntimeException {
 
-    public NoIdFoundException(String message) {
-        super(message);
+    public NoIdFoundException() {
+        super();
+        String message = "FATAL ERROR: OPERATION WITH NO EXISTING ID";
+        System.err.println(message);
     }
 }
